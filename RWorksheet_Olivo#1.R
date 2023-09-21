13537c8492b098c5db48387d289366640b23c466
Mel Adry Olivo <meladry.olivo@students.isatu.edu.ph>
2023-09-21 03:42:00 +0000

    updated worksheet #1

--- RWorksheet_Olivo#1.R
+++ RWorksheet_Olivo#1.R
@@ -1,3 +1,4 @@
+# MEL ADRY OLIVO BSIT 2C
 
 # 1
 # age vector
@@ -68,29 +69,67 @@ intFrom1to1000 <- seq(1,1000)
 intFrom1to1000
 
 # 8.a 
-lengthOf81 <- length(intFrom1to100)
-lengthOf81
-
-lengthOf82 <- length(numFrom20to60)
-lengthOf82
-
-lengthOf83 <- length(meanOfNumFrom20to60)
-lengthOf83
-
-lengthOf84 <- length(sumOfNumFrom51to91)
-lengthOf84
-
 # sum of all data points from 8.1 to 8.4
-allDataPoints <- lengthOf81 + lengthOf82 + lengthOf83 + lengthOf84
+allDataPoints <- length(intFrom1to100) + length(numFrom20to60) + length(meanOfNumFrom20to60) + length(sumOfNumFrom51to91)
 allDataPoints
 
 # 8.c
-
 new_85 <- seq(1,10)
 maxUntil10 <- max(new_85)
 maxUntil10
 
 # 9
-
 new_vec <- Filter(function(i) { all(i %% c(3,5,7) != 0) }, seq(100))
 new_vec
+
+# 10
+int_100_1 <- seq(100, 1)
+int_100_1
+
+# 11 
+natural_num_25 <- Filter(function(i) { all(i %% 3 == 0 || i %% 5 == 0) }, seq(24))
+natural_num_25
+sum_25 <- sum(natural_num_25)
+sum_25
+
+# 11.a
+totl_data_pts <- length(int_100_1) + length(natural_num_25) + length(sum_25)
+totl_data_pts
+
+# 12
+{ x <- 0 
+  + x + 5 + }
+
+# the code contains a syntax error
+# R reads it as two consecutive plus signs and a closing bracket 
+# without a corresponding opening bracket,
+# you can fix it by removing the extra plus sign at the end so it will
+# consider it as a block of code
+
+
+# 13
+score <- c(72, 86, 92, 63, 88, 89, 91, 92, 75, 75, 77)
+score
+
+element_2 <- score[2]
+element_3 <- score[3]
+
+element_2
+element_3
+
+# 14
+a = c(1,2,NA,4,NA,6,7)
+a
+print(a, na.print="999")
+
+# the code replaces NA with 999 and prints the vector "a"
+
+# 15
+name = readline(prompt="Input your name: ")
+age = readline(prompt="Input your age: ")
+print(paste("My name is",name, "and I am",age ,"years old."))
+print(R.version.string)
+
+# the output will depend on what the user's input for their name and age
+# after that, it will print a message with their inputted name and age
+# after that, it will print the R version information from "R.version.string"
